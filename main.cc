ac4f3b463fa022f9ad519124cf4f3a9121345ac8
Jake <jakswa@gmail.com>
2010-01-19 11:53:36 -0500

    editing looks

--- main.cc
+++ main.cc
@@ -1,13 +1,30 @@
 #include <gtkmm/main.h>
-#include "helloworld.h"
+#include <gtkmm/window.h>
+#include <gtkmm/button.h>
+#include <gtkmm/box.h>
+#include "myarea.h"
+#include <iostream>
 
 int main (int argc, char *argv[])
 {
   Gtk::Main kit(argc, argv);
 
-  HelloWorld helloworld;
-  //Shows the window and returns when it is closed.
-  Gtk::Main::run(helloworld);
+  Gtk::Window win;
+  win.set_border_width(10);
+  win.set_default_size(500,400);
+  win.set_title("Particle Display");
+  Gtk::HBox hbox(false, 5);
+  win.add(hbox);
+  
+  MyArea area;
+  hbox.pack_start(area);
+  area.show();
+  
+  Gtk::Button* pButton = new Gtk::Button("ooo");
+  hbox.pack_start(*pButton, Gtk::PACK_SHRINK);
+  (*pButton).show();
+  hbox.show();
+  Gtk::Main::run(win);
 
   return 0;
 }
